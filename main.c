d0c4679b3fcc94e76fe93e8ee7df0c01c4e39264
Michaël El Kharroubi <michael.elkh@gmail.com>
2018-06-24 20:47:12 +0200

    Amélioration générations Julia

--- main.c
+++ main.c
@@ -7,16 +7,15 @@
 void Generate_Julia_Set()
 {
     Matrix *julia;
-    int size = 500;
-    int iterations = 1<<10;
-    double complex constant; 
+    int size = 2048;
+    int iterations = 1<<11;
+    double complex constant;
     char name[37];
-    for(int i = -5; i < 6; i++)
+    for(int i = -10; i < 11; i++)
     {
-        constant = 0.2*i - (i>0 ? 0.01 : 0);
-        for(int j = 0; j < 5; j++)
+        for(int j = 0; j < 11; j++)
         {
-            constant = 0.2 * (i + j*I) - (i>0 ? 0.01 : 0); 
+            constant = 0.1 * (i + j*I) - (i>0 ? 0.01 : 0);
             julia = Get_Julia(size, iterations, constant);
             for(int k = 0; k<2; k++)
             {
@@ -28,7 +27,7 @@ void Generate_Julia_Set()
     }
 }
 int main()
-{    
+{
     Generate_Julia_Set();
     /*
     Matrix *res[2];
@@ -37,12 +36,12 @@ int main()
     double complex constant = -0.624 + 0.435*I;
     double complex center = -1.017838801 + -0.2830689082*I;
     double zoom = 1.5/2132;
-    
+
     int start = time(NULL);
     res[0] = Get_Julia(size, iterations, constant);
     res[1] = Get_Mandelbrot(size, iterations, center, zoom);
     printf("Temps de calculs : %ld\n", time(NULL)-start);
-    
+
     Save_Matrix_To_PNG(res[0], false, "Julia.png");
     Free_Matrix(res);
     Save_Matrix_To_PNG(res[1], false, "Mandelbrot.png");
